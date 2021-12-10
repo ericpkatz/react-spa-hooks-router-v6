9ea788f128c23139ee5213c0eb24bb3bb78eff8d
ericpkatz <ericpkatz@gmail.com>
2021-12-09 21:18:28 -0500

    commit

--- server.js
+++ server.js
@@ -2,14 +2,75 @@ const { syncAndSeed, models: { User, Thing} } = require('./db');
 const express = require('express');
 const app = express();
 const path = require('path');
+const jwt = require('jsonwebtoken');
 
 app.use('/dist', express.static(path.join(__dirname, 'dist')));
+app.use(express.json());
 
 app.get('/', (req, res)=> res.sendFile(path.join(__dirname, 'index.html')));
+
+app.get('/api/login', async(req, res, next)=> {
+  try {
+    const { id } = jwt.verify(req.headers.authorization, process.env.JWT);
+    res.send(await User.findByPk(id));
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+app.post('/api/login', async(req, res, next)=> {
+  try {
+    const { name } = req.body;
+    const user = await User.findOne({ where: { name }});
+    if(!user){
+      const err = Error('bad credentials');
+      err.status = 401;
+      throw err;
+    }
+    const token = jwt.sign({ id: user.id }, process.env.JWT);
+    res.send({ token });
+  }
+  catch(ex){
+    next(ex);
+  }
+});
 
 app.get('/api/users', async(req, res, next)=> {
   try {
     res.send(await User.findAll());
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+app.put('/api/users/:id', async(req, res, next)=> {
+  try {
+    const user = await User.findByPk(req.params.id);
+    await user.update(req.body);
+    res.send(user);
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+app.post('/api/users', async(req, res, next)=> {
+  try {
+    const user = await User.create(req.body); 
+    res.send(user);
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+
+app.delete('/api/users/:id', async(req, res, next)=> {
+  try {
+    const user = await User.findByPk(req.params.id);
+    await user.destroy();
+    res.sendStatus(204);
   }
   catch(ex){
     next(ex);
@@ -23,6 +84,11 @@ app.get('/api/things', async(req, res, next)=> {
   catch(ex){
     next(ex);
   }
+});
+
+app.use((err, req, res, next)=> {
+  console.log(err);
+  res.status(err.status || 500).send({ error: err.message });
 });
 
 const init = async()=> {
--- src/index.js
+++ src/index.js
@@ -1,10 +1,66 @@
 import axios from 'axios';
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { render } from 'react-dom';
 import { Provider, useSelector, useDispatch } from 'react-redux';
 import { combineReducers, createStore, applyMiddleware } from 'redux';
-import { NavLink, Outlet, Link, HashRouter, Routes, Route, useParams } from 'react-router-dom';
+import { useNavigate, NavLink, Outlet, Link, HashRouter, Routes, Route, useParams } from 'react-router-dom';
 import thunk from 'redux-thunk';
+
+const updateUser = (user)=> {
+  return async(dispatch)=> {
+    const response = await axios.put(`/api/users/${user.id}`, user);
+    dispatch({ type: 'UPDATE_USER', user: response.data });
+  };
+};
+
+const login = ({ name }, navigate)=> {
+  return async(dispatch)=> {
+    const response = await axios.post('/api/login', { name });
+    const { data: { token }} = response; 
+    window.localStorage.setItem('token', token);
+    dispatch(attemptTokenExchange());
+    navigate('/');
+  };
+};
+
+const logout = (navigate)=> {
+  return (dispatch)=> {
+    window.localStorage.removeItem('token');
+    dispatch({ type: 'SET_AUTH', auth: {} });
+    navigate('/login');
+  };
+};
+
+const attemptTokenExchange = ()=> {
+  return async(dispatch)=> {
+    const token = window.localStorage.getItem('token');
+    if(token){
+      const response = await axios.get('/api/login', { 
+        headers: {
+          authorization: token
+        }
+      });
+      const { data } = response; 
+      dispatch({ type: 'SET_AUTH', auth: data });
+    }
+  };
+};
+
+const deleteUser = (user, navigate)=> {
+  return async(dispatch)=> {
+    const response = await axios.delete(`/api/users/${user.id}`);
+    dispatch({ type: 'DELETE_USER', user });
+    navigate('/users');
+  };
+};
+
+const createUser = (user, navigate)=> {
+  return async(dispatch)=> {
+    const response = await axios.post('/api/users', user);
+    dispatch({ type: 'CREATE_USER', user: response.data });
+    navigate(`/users/${response.data.id}`);
+  };
+};
 
 const fetchThings = ()=> {
   return async(dispatch)=> {
@@ -27,24 +83,39 @@ const things = (state = [], action)=> {
   return state;
 };
 
+const auth = (state = {}, action)=> {
+  if(action.type === 'SET_AUTH'){
+    return action.auth;
+  }
+  return state;
+}
+
 const users = (state = [], action)=> {
   if(action.type === 'SET_USERS'){
     return action.users;
+  }
+  if(action.type === 'UPDATE_USER'){
+    return state.map(user => user.id !== action.user.id ? user : action.user);
+  }
+  if(action.type === 'DELETE_USER'){
+    return state.filter(user => user.id !== action.user.id); 
+  }
+  if(action.type === 'CREATE_USER'){
+    return [...state, action.user]; 
   }
   return state;
 };
 
 const store = createStore(combineReducers({
   things,
-  users
+  users,
+  auth
 }), applyMiddleware(thunk)); 
 
 const Main = ()=> {
   const dispatch = useDispatch();
-  useEffect(()=> {
-    dispatch(fetchThings());
-    dispatch(fetchUsers());
-  }, []);
+  const navigate = useNavigate();
+  const auth = useSelector(state => state.auth);
   const things = useSelector(state => state.things);
   const users = useSelector(state => state.users);
   return (
@@ -53,8 +124,16 @@ const Main = ()=> {
         <NavLink className={({isActive})=> isActive ? 'selected': '' } to='/'>Home</NavLink>
         <NavLink className={({isActive})=> isActive ? 'selected': '' }  to='/users'>Users ({ users.length})</NavLink>
         <NavLink className={({isActive})=> isActive ? 'selected': '' }  to='/things'>Things ({ things.length})</NavLink>
+        {
+        !auth.id  && <NavLink className={({isActive})=> isActive ? 'selected': '' }  to='/login'>Login</NavLink>
+        }
+        {
+        !!auth.id  && <button onClick={ ()=> dispatch(logout(navigate)) }>Logout</button>
+        }
       </nav>
-      <Outlet />
+      <main>
+        <Outlet />
+      </main>
     </div>
   );
 };
@@ -62,9 +141,21 @@ const Main = ()=> {
 
 const Users = ()=> {
   const users = useSelector(state => state.users);
+  const [name, setName ] = useState('');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const submit = (ev)=> {
+    ev.preventDefault();
+    dispatch(createUser({ name }, navigate));
+    setName('');
+  };
   return (
     <div>
       <h2>Users</h2>
+      <form>
+        <input value={ name } onChange={ ev => setName(ev.target.value)} /> 
+        <button onClick={ ev => submit(ev)}>Create</button>
+      </form>
       <ul>
         {
           users.map( user => {
@@ -83,10 +174,27 @@ const Users = ()=> {
 
 const User = ()=> {
   const { id } = useParams();
-  const user = useSelector(state => state.users.find( user => user.id === id*1));
+  const user = useSelector(state => state.users.find( user => user.id === id*1)) || {};
+  const [ name, setName ] = useState('');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  useEffect(()=> {
+    if(user.id){
+      setName(user.name);
+    }
+  }, [ user ]);
+  const submit = (ev) => {
+    ev.preventDefault();
+    dispatch(updateUser({ name, id}));
+  }
   return (
     <div>
       <h2>User</h2>
+    <form>
+      <input value={ name } onChange={ev => setName(ev.target.value)} />
+      <button onClick={ (ev)=> submit(ev) } disabled={ user.name === name }>Update</button>
+    </form>
+    <button onClick={()=> dispatch(deleteUser(user, navigate))}>Delete</button>
     <pre>
 {
   JSON.stringify(user || {}, null, 2)
@@ -131,22 +239,67 @@ const Things = ()=> {
   );
 }
 const Home = ()=> <div>Home</div>;
+const Login = ()=> {
+  const [name, setName] = useState('');
+  const [error, setError] = useState('');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const submit = async(ev)=> {
+    try {
+      ev.preventDefault();
+      await dispatch(login({ name }, navigate));
+    }
+    catch(ex){
+      setError(ex.response.data.error);
+    }
+
+  };
+  return (
+    <form>
+      { !!error && <div className='error'>{ error }</div> }
+      <input value={ name } onChange={ ev => setName(ev.target.value)} /> 
+      <button onClick={ submit }>Login</button>
+    </form>
+  );
+}; 
+
+const AppRoutes = ()=> {
+  const dispatch = useDispatch();
+  const auth = useSelector(state => state.auth);
+
+  useEffect(()=> {
+    try {
+      dispatch(attemptTokenExchange());
+    }
+    catch(ex){
+      console.log(ex);
+    }
+  }, []);
+  useEffect(()=> {
+    dispatch(fetchThings());
+    dispatch(fetchUsers());
+  }, []);
+  return (
+    <Routes>
+      <Route path='/' element={ <Main />} >
+        <Route path='users' element={ <Users /> }>
+          <Route path=':id' element={<User />} />
+        </Route>
+        <Route path='things' element={ <Things /> }>
+          <Route path=':id' element={ <Thing /> } />
+        </Route>
+        { !auth.id && <Route path='login' element={ <Login /> } /> }
+        <Route path='' element={ <Home /> } />
+      </Route>
+    </Routes>
+  );
+};
 
 const App = ()=> {
   return (
     <Provider store={ store }>
       <HashRouter>
-        <Routes>
-          <Route path='/' element={ <Main />} >
-            <Route path='users' element={ <Users /> }>
-              <Route path=':id' element={<User />} />
-            </Route>
-            <Route path='things' element={ <Things /> }>
-              <Route path=':id' element={ <Thing /> } />
-            </Route>
-            <Route path='' element={ <Home /> } />
-          </Route>
-        </Routes>
+        <AppRoutes />
       </HashRouter>
     </Provider>
   );
